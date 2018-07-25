7e60325edc22e849b0051caea2b80c76a9f2e0b0
Fischer, Kevin A <Kevin.A.Fischer@HealthPartners.Com>
2018-07-24 21:02:00 -0500

    Add some crap

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+cars = ["Ford", "Volvo", "BMW", "Toyota"]
+
+response1 = 'I am well'
+response2 = 'Lets eat dinner twice'
+response3 = 'I have a sore foot'
+response4 = 'Inside.  Somewhere.  Alone.'
+
+
+response = 'I am well.'
+
+question = question1
+
+print(response)

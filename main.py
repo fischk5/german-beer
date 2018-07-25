a64e6ac970bb7598e13b9b3094a1447b11da32ab
Fischer, Kevin A <Kevin.A.Fischer@HealthPartners.Com>
2018-07-24 21:28:43 -0500

    Add question loop for fun

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-
-
-cars = ["Ford", "Volvo", "BMW", "Toyota"]
+# Lets play question and response
+import random
 
 response1 = 'I am well'
 response2 = 'Lets eat dinner twice'
 response3 = 'I have a sore foot'
 response4 = 'Inside.  Somewhere.  Alone.'
+response5 = 'Its about time you asked me about that'
+response6 = 'Probably check your pocket.'
+response7 = 'Yes'
+response8 = 'As far as I can tell, it is cold.'
 
+responses = [response1, response2, response3, response4, response5, response6, response7, response8]
 
-response = 'I am well.'
+question = input("####\n")
 
-question = question1
-
+while question != 'exit':
+    responseInt=random.randint(0,len(responses))
+    print(responses[responseInt])
+    question = input("####\n")
 print(response)

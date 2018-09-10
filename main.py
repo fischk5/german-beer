acd848240225d5802a1857cc9ba48afe5fc18d46
Fischer, Kevin A <Kevin.A.Fischer@HealthPartners.Com>
2018-09-10 15:38:43 -0500

    Add two methods for creating sentences and a TODO for structural analysis

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import random
 from nltk import word_tokenize
 
 tagDict = {}
-adverb = []
-noun = []
-preposition = []
-adjective = []
-verb = []
-coordinatingConjunction = []
-determiner = []
-cardinalDigit = []
-existentialThere = []
-foreignWord = []
-adjectiveComparative = []
-adjectiveSuperlative = []
+adverb = [] # describes a verb, adjective, or another adverb
+noun = [] # person place, thing, or idea
+preposition = [] # a word governing, and usually preceding, a noun or pronoun and expressing a relation to another word or element in the clause, as in “the man on the platform,” “she arrived after dinner,”
+adjective = [] # describes a noun or pronoun
+verb = [] # An action
+coordinatingConjunction = [] # join two main clauses to make a complete sentence
+determiner = [] # determines the kind of reference a noun or noun group has, for example a, the, every
+cardinalDigit = [] # number
+existentialThere = [] #  a clause that refers to the existence or presence of something. Examples in English include the sentences "There is a God" and "There are boys in the yard".
+foreignWord = [] # Foreign word
+adjectiveComparative = [] # They are used in sentences where two nouns are compared
+adjectiveSuperlative = [] # A superlative adjective compares three or more nouns
 listMarker = []
 modal = []
 nounPlural = []
@@ -29,21 +29,32 @@ possessivePronoun = []
 particle = []
 to = []
 uh = []
+verbBase = []
 verbPast = []
 verbGerund = []
 verbPastParticiple = []
 verbZ = []
 whDeterminer = []
 whPossessivePronoun = []
+whPronoun = []
 whAdverb = []
 
-url = "http://www.gutenberg.org/files/5200/5200.txt"
+url = "https://www.gutenberg.org/files/1342/1342-0.txt"
 script = requests.get(url)
 script = script.text
+script = script.replace("'", "")
+script = script.replace(",", "")
+script = script.replace("-", "")
+script = script.replace(";", ".")
+script = script.replace(":", "")
+script = script.replace("_", "")
 sentenceStructures = []
+
+exStructure = [["DT", "JJ", "NN", "VBD"], ["DT", "NN", "VBD"]]
 
 tagDict["CC"] = coordinatingConjunction
 tagDict["RB"] = adverb
+tagDict["VB"] = verbBase
 tagDict["IN"] = preposition
 tagDict["NN"] = noun
 tagDict["JJ"] = adjective
@@ -72,9 +83,16 @@ tagDict["VBN"] = verbPastParticiple
 tagDict["VBZ"] = verbZ
 tagDict["WDT"] = whDeterminer
 tagDict["WP$"] = whPossessivePronoun
+tagDict["WP"] = whPronoun
 tagDict["WRB"] = whAdverb
 
 sentence = "Although Gregor wasn't able to hear any news directly he did listen to much of what was said in the next rooms, and whenever he heard anyone speaking he would scurry straight to the appropriate door and press his whole body against it.  There was seldom any conversation, especially at first, that was not about him in some way, even if only in secret.  For two whole days, all the talk at every mealtime was about what they should do now; but even between meals they spoke about the same subject as there were always at least two members of the family at home - nobody wanted to be at home by themselves and it was out of the question to leave the flat entirely empty.  And on the very first day the maid had fallen to her knees and begged Gregor's mother to let her go without delay.  It was not very clear how much she knew of what had happened but she left within a quarter of an hour, tearfully thanking Gregor's mother for her dismissal as if she had done her an enormous service."
+
+sentence = sentence.replace("'", "")
+sentence = sentence.replace(",", "")
+sentence = sentence.replace("-", "")
+sentence = sentence.replace(";", ".")
+sentence = sentence.replace(":", "")
 
 def addLexicalValue(tagged, target):
     if tagged[1] == target and tagged[0] not in tagDict[target] and not tagged[0].isupper():
@@ -101,7 +119,7 @@ def studySentenceStructures(input):
             structure = []
         index += 1
 
-def genSentence(input):
+def genSentenceFromStructure(input):
     chosenStructure = random.choice(sentenceStructures)
     newSentence = ""
     index = 0
@@ -119,10 +137,37 @@ def genSentence(input):
     print(newSentence)
     print("")
 
-def addSentences(amount, source):
+def addSentencesFromStructure(amount, source):
     for number in range(amount):
-        genSentence(source)
+        genSentenceFromStructure(source)
 
-buildLibrary(sentence)
-studySentenceStructures(sentence)
-addSentences(4, sentence)
+def genSentenceNoStructure(input):
+    structure = random.choice(exStructure)
+    #structure = exStructure[1]
+    index = 0
+    newSentence = ""
+    while index < len(structure):
+        tag = structure[index]
+        word = random.choice(tagDict[tag])
+        if index == 0:
+            word = word.lower().capitalize()
+            newSentence += word
+        else:
+            word = word.lower()
+            newSentence += " " + word
+        index += 1
+    newSentence += "."
+    print(newSentence)
+    print("")
+
+def addSentencesNoStructure(amount, source):
+    for number in range(amount):
+        genSentenceNoStructure(source)
+
+buildLibrary(script)
+studySentenceStructures(script)
+addSentencesFromStructure(25, script)
+#addSentencesNoStructure(15, script)
+
+## TODO: Capture groups of words together instead of individual words and buildLibrary
+## structures based off that instead
